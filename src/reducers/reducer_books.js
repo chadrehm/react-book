d29e836991b63de2f6a914ac76231d20b85c0864
chad rehm <chad.rehm@kineticdata.com>
2017-05-21 11:05:07 -0500

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title:'Batman', pages: 123},
+    {title:'Super Man', pages:23},
+    {title:'The Flash', pages: 343},
+    {title:'Martian man hunter', pages: 232}
+  ]
+}
